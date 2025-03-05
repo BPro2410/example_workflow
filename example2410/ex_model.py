08b8acb35446e8041beab19b89597e9784cd94fd
Bernd Prostmaier <b.prostmaier@icloud.com>
2025-03-05 21:14:21 +0100

    test2433

--- example2410/ex_model.py
+++ example2410/ex_model.py
@@ -3,6 +3,7 @@ from example2410.helper import helper
 
 class sum_tf:
     def __init__(self, numbers):
+        print("HELLO")
         for i in numbers:
             helper.checknumber(i)
         self.sum = tf.reduce_sum(numbers)
