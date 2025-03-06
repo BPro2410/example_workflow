593e47fbe9f290e4c60729a194f5ac0bd78707c2
Bernd Prostmaier <b.prostmaier@icloud.com>
2025-03-06 18:23:27 +0100

    test

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
 #
+
+print("HELLO")
+
+import numpy as np
+np.__version__
+
+import pandas as pd
+pd.__version__
+
+import matplotlib
+matplotlib.__version__
+
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+tf.__version__
+
+import tensorflow_probability as tfp
